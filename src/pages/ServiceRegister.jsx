c482f766a6fbad6bdaf225b01f219a1255dbd082
R. A. B. Jinajith <131400361+bosilu-ranathunga@users.noreply.github.com>
2025-03-30 18:28:53 +0530

    Delete src directory

--- src/pages/ServiceRegister.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ServiceRegister() {
-    return (
-        <div>ServiceRegister</div>
-    )
-}
--- src/pages/business/AddBusiness.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function AddBusiness() {
-    return (
-        <div>AddBusiness</div>
-    )
-}
