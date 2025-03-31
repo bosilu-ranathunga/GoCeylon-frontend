6743890b039636bde8e26ad74a7a8d03bf98f489
Bosilu Ranathunga <rabjinajith@gmail.com>
2025-04-01 03:05:09 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,5 @@
-const API_BASE_URL = "http://192.168.8.112:3000";
+//const API_BASE_URL = "http://192.168.8.112:3000";
+const API_BASE_URL = "https://goceylon-effhf6gxg5bqachv.westindia-01.azurewebsites.net";
+
 
 export default API_BASE_URL;
